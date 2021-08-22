1af08bed6542b525e5b5d18de7b88781c4d1e2f4
lucastseun <lucastseun@qq.com>
2021-08-22 12:22:45 +0800

    修改

--- dbsource/mysql.go
+++ dbsource/mysql.go
@@ -8,14 +8,14 @@ import (
 )
 
 const (
-	user     = "root"
-	password = "123456"
-	ip       = "127.0.0.1"
-	dbname   = "ituring"
+	USER     = "root"
+	PASSWORD = "123456"
+	IP       = "127.0.0.1"
+	DBNAME   = "ituring"
 )
 
 func ConnectMYSQL() (*gorm.DB, error) {
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s?charset=utf8mb4&parseTime=True&loc=Local", user, password, ip, dbname)
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s?charset=utf8mb4&parseTime=True&loc=Local", USER, PASSWORD, IP, DBNAME)
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
